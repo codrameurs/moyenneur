f921bb1c9ec32ccf23f58972fe0e65f3cbc025bc
zarthurax <49124187+zarthurax@users.noreply.github.com>
2020-01-04 16:39:21 +0100

    ajout de le fonction getdict
    sert a recup le dict des notes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from moyenneur_module import *
 '''
-c'est le code
+
 '''
 
 class Fenetre():
@@ -10,6 +10,7 @@ class Fenetre():
         self.fenetre = Tk()
         self.fenetre.title("Moyeneur")
         self.fenetre.geometry("700x550")
+        self.notes=getdict()
     def valider_note(self):
         note = entry.get()
         print(note)
@@ -22,21 +23,25 @@ class Fenetre():
     def menu_princ(self):
         note=0
         sur=0
-        title=Label(fenetre,text="Bienvenue sur le moyeneur")
+        title=Label(self.fenetre,text="Bienvenue sur le moyeneur")
         title.place(x=300,y=0)
-        entry=Entry(fenetre)
+        print(self.notes)
+        """
+        entry=Entry(self.fenetre)
         entry.place(x=300,y=50)
-        enter=Button(fenetre,text="entrer",command=valider_note)
+        enter=Button(self.fenetre,text="entrer",command=self.valider_note)
         enter.place(x=300,y=100)
-        phraseN=Label(fenetre,text="entrez votre note:")
+        phraseN=Label(self.fenetre,text="entrez votre note:")
         phraseN.place(x=300,y=30)
-        phraseS=Label(fenetre,text="Sur combien est votre notre:")
+        phraseS=Label(self.fenetre,text="Sur combien est votre notre:")
         phraseS.place(x=300,y=30)
-        enterS=Button(fenetre,text="entrer",command=valider_sur)
+        enterS=Button(self.fenetre,text="entrer",command=self.valider_sur)
         enterS.place(x=300,y=100)
-        texte = Label(fenetre,text="%d/%d"%(note,sur))
+        texte = Label(self.fenetre,text="%d/%d"%(note,sur))
         texte.place(x=20,y=100)
-        fenetre.mainloop()
+        """
+        self.fenetre.mainloop()
 
 
 fenetre=Fenetre()
+fenetre.menu_princ()
--- moyenneur_module.py
+++ moyenneur_module.py
@@ -1,10 +1,17 @@
 '''
 
 '''
-
 def save(dict):
+    # sauve les notes dans notes.json
     fichier=open("note.json","w")
     dictstr=str(dict)
     dictstr=dictstr.replace("'",'"')
     fichier.write(dictstr)
     fichier.close
+
+def getdict():
+    #recupere les notes de notes.json
+    import json
+    with open("notes.json") as f:
+        notes_dict = json.load(f)
+    return notes_dict
