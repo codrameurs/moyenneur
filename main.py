18a6c151590e37b6deb4fbf8f9ef624aa67af3d6
Nonins <aissani.nacim@gmail.com>
2020-02-18 12:54:17 +0100

    ajout d'une variable valider_sur et debogage de la fenetre

--- main.py
+++ main.py
@@ -11,37 +11,38 @@ class Fenetre():
         self.fenetre.title("Moyeneur")
         self.fenetre.geometry("700x550")
         self.notes=getdict()
-    def valider_note(self):
+        self.menu_princ()
+
+    def valider_note(self,entry):
         note = entry.get()
         print(note)
         return note
-    def valider_sur(self):
-        sur=entry.get()
-        phraseS.pack()
-        print=(sur)
+
+    def valider_sur(self,entry):
+        sur = entry.get()
+        print(sur)
         return sur
+
     def menu_princ(self):
         note=0
         sur=0
         title=Label(self.fenetre,text="Bienvenue sur le moyeneur")
         title.place(x=300,y=0)
         print(self.notes)
-        """
         entry=Entry(self.fenetre)
         entry.place(x=300,y=50)
-        enter=Button(self.fenetre,text="entrer",command=self.valider_note)
+        enter=Button(self.fenetre,text="entrer",command=self.valider_note(entry))
         enter.place(x=300,y=100)
         phraseN=Label(self.fenetre,text="entrez votre note:")
         phraseN.place(x=300,y=30)
         phraseS=Label(self.fenetre,text="Sur combien est votre notre:")
         phraseS.place(x=300,y=30)
-        enterS=Button(self.fenetre,text="entrer",command=self.valider_sur)
+        enterS=Button(self.fenetre,text="entrer",command=self.valider_sur(entry))
         enterS.place(x=300,y=100)
         texte = Label(self.fenetre,text="%d/%d"%(note,sur))
         texte.place(x=20,y=100)
-        """
         self.fenetre.mainloop()
 
-
+        
 fenetre=Fenetre()
-fenetre.menu_princ()
+fenetre.__init__()
